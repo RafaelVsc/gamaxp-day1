160d3255dc411b80fd35d987a0db95dd2b2a25c1
Leonardo Santos Severino <leonardossev@gmail.com>
2020-01-25 10:10:06 -0300

    Adds a function that returns jokes based on a text

--- src/api.js
+++ src/api.js
@@ -4,6 +4,7 @@ const CONTENT_TYPE = 'application/json';
 const MODE = 'cors';
 
 let isFetchingRandomJoke = false;
+let isSearchingJokes = false;
 
 async function getRandomJoke() {
   isFetchingRandomJoke = true;
@@ -22,5 +23,25 @@ async function getRandomJoke() {
     console.error(error);
   } finally {
     isFetchingRandomJoke = false;
+  }
+}
+
+async function searchJokes(text) {
+  isSearchingJokes = true;
+
+  try {
+    const response = await fetch(`${BASE_URL}search?query=${text}`, {
+      method: METHOD,
+      mode: MODE,
+      headers: {
+        'Content-Type': CONTENT_TYPE
+      }
+    });
+
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    isSearchingJokes = false;
   }
 }
