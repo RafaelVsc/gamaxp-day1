e4016a86511dca4070ab592938696084f3dc4db8
Leonardo Santos Severino <leonardossev@gmail.com>
2020-01-25 10:00:07 -0300

    WIP: getRandomJoke api

--- src/api.js
+++ src/api.js
@@ -1,3 +1,17 @@
 const BASE_URL = 'https://api.chucknorris.io/jokes/';
 const METHOD = 'GET';
 const CONTENT_TYPE = 'application/json';
+const MODE = 'cors';
+
+function getRandomJoke() {
+  fetch(`${BASE_URL}random`, {
+    method: METHOD,
+    mode: MODE,
+    headers: {
+      'Content-Type': CONTENT_TYPE
+    }
+  })
+  .then(response => {
+    console.log(response.json());
+  });
+}
